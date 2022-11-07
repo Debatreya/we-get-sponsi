bd51688c88e9eaf7483ca2cabdb9c46079ed4ada
Yash Mittal <mittal.yash12@hotmail.com>
2022-11-07 23:42:13 +0530

    Fix: Email Subject and error handling in sending
    Signed-off-by: Yash Mittal <mittal.yash12@hotmail.com>

--- pages/api/sendmail.ts
+++ pages/api/sendmail.ts
@@ -32,43 +32,31 @@ const handleMailingRequest = async (req: NextApiRequest, res: NextApiResponse) =
     refresh_token: token?.refresh_token,
   }
   );
+  try {
+    await Promise.all(emails.map(async (email: string) => {
+      const options = {
+        to: email,
+        cc: cc.join(', '),
+        replyTo: token?.email,
+        subject: "Regarding Sponsorship for Techspardha'23 @NITKurukshetra",
+        text: mailContent,
+        // html: `<p>${mailContent}</p>`,
+        attachments: mailAttachments,
+        textEncoding: "base64",
+        headers: [
+          { key: "X-Application-Developer", value: "Yash Mittal" },
+          { key: "X-Application-Version", value: "v1.0.0.2" },
+        ],
+      }
+      const message = await createMail(options);
 
-  return await Promise.all(emails.map(async (email: string) => {
-    const options = {
-      to: email,
-      cc: cc.join(', '),
-      replyTo: token?.email,
-      subject: "Hello From We-Get-Sponsi🚀",
-      text: mailContent,
-      // html: `<p>${mailContent}</p>`,
-      attachments: mailAttachments,
-      textEncoding: "base64",
-      headers: [
-        { key: "X-Application-Developer", value: "Yash Mittal" },
-        { key: "X-Application-Version", value: "v1.0.0.2" },
-      ],
-    }
-    const message = await createMail(options);
-    return sendMail(gmail, message);
-  }))
+      return sendMail(gmail, message);
+    }))
 
-  /*const options = {
-    to: "yami8b@gmail.com",
-    cc: "techsavvyash@gmail.com, yaadonkabackup@gmail.com",
-    replyTo: "yash_12012002@nitkkr.ac.in",
-    subject: "Hello From We-Get-Sponsi🚀",
-    text: "This email is sent from the command line",
-    html: `<p>🙋🏻‍♀️  &mdash; This is a <b>test email</b> from my mailer.</p>`,
-    attachments: mailAttachments,
-    textEncoding: "base64",
-    headers: [
-      { key: "X-Application-Developer", value: "Yash Mittal" },
-      { key: "X-Application-Version", value: "v1.0.0.2" },
-    ],
-  };
-  const rawMessage = await createMail(options);
-  return await sendMail(gmail, rawMessage);*/
-  // return "done";
+    res.json("Emails Sent Successfully");
+  } catch (err) {
+    res.json({ error: err });
+  }
 }
 
 export default handleMailingRequest;  
